2ded83adcc54da04c28f8b40f99be17f455c6721
hp <57030669+hammad8634@users.noreply.github.com>
2023-04-11 19:23:16 +0500

    updated footer and all other components

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -6,18 +6,59 @@ import "../App.css";
 const BlogFooter = () => {
   return (
     <div className="footer-main-container">
-      {" "}
       <footer className=" text-black py-5 footer-container">
         <Container className="justify-content-center">
           <Row>
             <Col md={4}>
               <h4>Shifa</h4>
-              <p className="fs-6">
+              <p className="fs-6 ">
                 We are creating High Quality Resources and tools to Aid
-                developers during the developement of their projects
+                developers during the development of their projects.
               </p>
             </Col>
-            <Col md={4}>
+            <Row className="d-md-none">
+              <Col xs={6}>
+                <h4>Doctor</h4>
+                <ul className="list-unstyled">
+                  <li>
+                    <a href="#" className="footer-list">
+                      Resources
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="footer-list">
+                      About Us
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="footer-list">
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </Col>
+              <Col xs={6}>
+                <h4>Help</h4>
+                <ul className="list-unstyled">
+                  <li>
+                    <a href="#" className="footer-list">
+                      Support
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="footer-list">
+                      Sign Up
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="footer-list">
+                      Sign In
+                    </a>
+                  </li>
+                </ul>
+              </Col>
+            </Row>
+            <Col md={4} className="d-none d-md-block">
               <h4>Doctor</h4>
               <ul className="list-unstyled">
                 <li>
@@ -37,7 +78,7 @@ const BlogFooter = () => {
                 </li>
               </ul>
             </Col>
-            <Col md={4}>
+            <Col md={4} className="d-none d-md-block">
               <h4>Help</h4>
               <ul className="list-unstyled">
                 <li>
@@ -57,7 +98,7 @@ const BlogFooter = () => {
                 </li>
               </ul>
             </Col>
-          </Row>{" "}
+          </Row>
         </Container>
         <div className="d-flex justify-content-center">
           <Row className="mt-3">
