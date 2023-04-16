5a97c16b2c621ed000e6e0e1c63a341b948b3802
hp <57030669+hammad8634@users.noreply.github.com>
2023-04-16 18:36:36 +0500

    ..

--- src/Components/intro.js
+++ src/Components/intro.js
@@ -32,43 +32,47 @@ function Intro() {
   const classes = useStyles();
 
   return (
-    <Container fluid className="intro-main-container">
-      <Grid container>
-        {/* Left column */}
-        <Grid item xs={12} sm={6}>
-          <div className={classes.container}>
-            <Typography variant="h5" className={classes.text}>
-              Pakistan's Leading Bariatric <br /> & <br /> Laparascopic Surgeon{" "}
-            </Typography>
-          </div>
-        </Grid>
-
-        {/* Right column */}
-        <Grid item xs={12} sm={6}>
-          <img src={img1} className={classes.img} alt="Responsive" />
-        </Grid>
-      </Grid>
-
-      <div className="py-5 intro-container-2">
-        <Row className="align-items-center">
-          <Col md={6} className="intro-text">
-            <div className="text-container-intro">
-              <h2 className="text-center text-md-center mb-0 text-before-award">
-                Professor & Chief of Surgery{" "}
-              </h2>
-              <p className="shifa-p">
-                {" "}
-                Shifa International Hospital, Islamabad
-              </p>
+    <>
+      {" "}
+      <Container fluid className="intro-main-container">
+        <Grid container>
+          {/* Left column */}
+          <Grid item xs={12} sm={6}>
+            <div className={classes.container}>
+              <Typography variant="h5" className={classes.text}>
+                Pakistan's Leading Bariatric <br /> & <br /> Laparascopic
+                Surgeon{" "}
+              </Typography>
             </div>
-          </Col>
-          <Col>
-            {" "}
-            <Awards />
-          </Col>{" "}
-        </Row>
-      </div>
-    </Container>
+          </Grid>
+
+          {/* Right column */}
+          <Grid item xs={12} sm={6}>
+            <img src={img1} className={classes.img} alt="Responsive" />
+          </Grid>
+        </Grid>
+
+        <div className="py-5 intro-container-2" id="my-div">
+          <Row className="align-items-center">
+            <Col md={6} className="intro-text">
+              <div className="text-container-intro">
+                <h2 className="text-center text-md-center mb-0 text-before-award">
+                  Professor & Chief of Surgery{" "}
+                </h2>
+                <p className="shifa-p">
+                  {" "}
+                  Shifa International Hospital, Islamabad
+                </p>
+              </div>
+            </Col>
+            <Col>
+              {" "}
+              <Awards />
+            </Col>{" "}
+          </Row>
+        </div>
+      </Container>
+    </>
   );
 }
 
